6aed180ef643db331074ff26185b87e5223c7ea8
iSerje812 <140149493+iSerje812@users.noreply.github.com>
2023-08-25 00:00:40 +0300

    task17 added

---
+++ task17/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.WriteLine("Введите x y");
+int userX = Convert.ToInt32(Console.ReadLine());
+int userY = Convert.ToInt32(Console.ReadLine());
+
+
+if (userX > 0 && userY > 0)
+{
+    System.Console.WriteLine($"Точка [{userX} : {userY}] находится в первой четверти");
+}
+if (userX < 0 && userY > 0)
+{
+    System.Console.WriteLine($"Точка [{userX} : {userY}] находится в второй четверти");
+}
+if (userX < 0 && userY < 0)
+{
+    System.Console.WriteLine($"Точка [{userX} : {userY}] находится в третьей четверти");
+}
+else
+{
+    System.Console.WriteLine($"Точка [{userX} : {userY}] находится в четвертой четверти");
+}
