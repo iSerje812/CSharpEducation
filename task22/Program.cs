bcad07cceec15e61026aa199f3de5337f2fe09d5
iSerje812 <140149493+iSerje812@users.noreply.github.com>
2023-08-25 01:39:53 +0300

    task22 added

---
+++ task22/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите прогу которая на ввод принимает число N и выдает таблицу квадратов чисел от 1 до N
+*/
+
+using System.Runtime.Serialization.Formatters;
+
+Console.WriteLine("Введите число N:");
+
+int N = Convert.ToInt32(Console.ReadLine());
+double result = 0;
+if (N < 0)
+{
+    N = N * (-1);
+}
+for (int i = 1; i <= N; i++)
+{
+    result = Math.Pow(i, 2);
+    Console.WriteLine(result);
+}
