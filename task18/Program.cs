d451f4f15c91c15a8dcaec906e96644eaa8c785b
iSerje812 <140149493+iSerje812@users.noreply.github.com>
2023-08-25 00:33:40 +0300

    Task18 added

---
+++ task18/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите прогу которая по заданному номеру четверти показавыет диапазон возможных координат этой точки//
+Console.WriteLine("Введите номер четверти");
+int numberChetverti = Convert.ToInt32(Console.ReadLine());
+
+if (numberChetverti == 1)
+{
+    Console.WriteLine("x > 0, y > 0");
+}
+else if (numberChetverti == 2)
+{
+    Console.WriteLine("x < 0, y > 0");
+}
+else if (numberChetverti == 3)
+{
+    Console.WriteLine("x < 0, y < 0");
+}
+else if (numberChetverti == 4)
+{
+    Console.WriteLine("x > 0, y < 0");
+}
+else Console.WriteLine("Нет такой четверти");
