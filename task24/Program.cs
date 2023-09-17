278d5f32690c1f7798ba8a6a730a1668e96b022d
iSerje812 <140149493+iSerje812@users.noreply.github.com>
2023-09-17 11:45:02 +0300

    task30 added

---
+++ task24/Program.cs
@@ -0,0 +1,22 @@
+﻿// 24. Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.//
+System.Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+for (int i = 0; i <= userNumber; i++)
+{
+    sum += i;
+}
+System.Console.WriteLine(sum);
+
+int SumOfNumber(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= userNumber; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine($"Сумма цифр в числе {userNumber} равна {SumOfNumber(userNumber)}");
---
+++ task30/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+30. Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+*/
+/*
+int[] GetRundomArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0,2);
+
+    }
+    return array;
+}
+
+int[] userArray = GetRundomArray();
+
+void PrintArray(int[]arrayToPrint)
+{
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        System.Console.WriteLine($"({arrayToPrint[i]} )");
+    }
+}
+PrintArray(userArray);
+*/
+
+/* 30/ Напишите программу которая выводит массив из 8 элементов, заполненный 0 и 1 в случайном порядке*/
+
+int[] GetRandomArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+
+    }
+    return array;
+}
+int[] userArray = GetRandomArray();
+
+void PrintArray(int[] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        System.Console.Write($"{arrayToPrint[i]} ");
+    }
+    Console.Write("]");
+}
+
+PrintArray(userArray);
