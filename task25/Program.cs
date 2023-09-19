2df36dcd8454c72893cba1f2ac67555478b7cdf2
iSerje812 <140149493+iSerje812@users.noreply.github.com>
2023-09-19 03:25:37 +0300

    task 25 loop solution added

--- task25/Program.cs
+++ task25/Program.cs
@@ -3,6 +3,8 @@
 2, 4 -> 16
 */
 
+/* Решение через метод:
+
 double EponentiationOpetator(int numberA, int numberB)
 {
     double result = Math.Pow(numberA, numberB);
@@ -15,10 +17,27 @@ int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число B: ");
 int B = Convert.ToInt32(Console.ReadLine());
 
-double exponentiation = EponentiationOpetator(A, B);
-
-
 //Console.WriteLine(EponentiationOpetator(A, B));
+
+double exponentiation = EponentiationOpetator(A, B);
 
 Console.WriteLine($"Результат возведения числа {A} в степень {B} равен: {exponentiation}");
 Console.WriteLine($"{B} степень числа {A} = {exponentiation}");
+*/
+
+/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int exponentiation = A;
+for (int i = 1; i < B; i++)
+{
+    exponentiation *= A;
+}
+Console.WriteLine(exponentiation);
