911ae0207f67848d8b3e5dcb32ec77bab27c511e
iSerje812 <140149493+iSerje812@users.noreply.github.com>
2023-09-19 03:43:52 +0300

    task27 added

---
+++ task27/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int figure = 0;
+for (int i = number; i > 0; i= i / 10 )
+{
+    figure = i % 10 + figure;
+}
+
+Console.WriteLine(figure);
