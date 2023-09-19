aeaeb68629af3b383e8fbf23f723a595b8b16a2b
iSerje812 <140149493+iSerje812@users.noreply.github.com>
2023-09-19 21:25:03 +0300

    task29 added

---
+++ task29/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Введите длину и границы чисел массива: ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] GetRandomArray(int arrayLength)
+
+{
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+int[] userArray = GetRandomArray(arrayLength);
+
+void PrintArray(int[] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        System.Console.Write($"{arrayToPrint[i]} ");
+    }
+    Console.Write("]");
+}
+
+PrintArray(userArray);
